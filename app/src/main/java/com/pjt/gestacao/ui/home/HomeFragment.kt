fd8e7c30039803fdd8b9f8ccb2224ec856c7f561
vmila1 <vitoriacamila1@hotmail.com>
2025-05-08 18:02:41 -0300

    feat: adiciona ideia de layout inicial

--- app/src/main/java/com/pjt/gestacao/ui/home/HomeFragment.kt
+++ app/src/main/java/com/pjt/gestacao/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,28 +13,35 @@ import com.pjt.gestacao.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var homeViewModel: HomeViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        // Exemplo: atualizando o TextView com o valor do ViewModel
+        val gestationTextView: TextView = binding.textView
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            gestationTextView.text = it
         }
-        return root
+
+        // Exemplo: clicando no botão
+        binding.button.setOnClickListener {
+            // Aqui você pode executar alguma ação
+            // Por exemplo: mostrar uma mensagem ou navegar
+            homeViewModel.updateText("8 Meses")
+        }
+
+        return binding.root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/pjt/gestacao/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/pjt/gestacao/ui/home/HomeViewModel.kt
@@ -7,7 +7,11 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "7 meses"
     }
     val text: LiveData<String> = _text
+
+    fun updateText(newText: String) {
+        _text.value = newText
+    }
 }
